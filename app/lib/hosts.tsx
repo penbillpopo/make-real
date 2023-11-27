4b899b3a65ac1c534cb783353e009e69ad386083
bill <bill2021@anyong.com.tw>
2023-11-27 15:44:29 +0800

    修正dev

--- app/lib/hosts.tsx
+++ app/lib/hosts.tsx
@@ -8,13 +8,13 @@ const env =
 export const LINK_HOST = {
 	production: 'makereal.imaginetw.com',
 	preview: `link---${process.env.NEXT_PUBLIC_VERCEL_URL}`,
-	development: 'localhost.localhost',
+	development: 'localhost.localhost:3000',
 }[env]
 
 export const APP_HOST = {
 	production: 'admin.imaginetw.com',
 	preview: process.env.NEXT_PUBLIC_VERCEL_URL,
-	development: 'localhost',
+	development: 'localhost:3000',
 }[env]
 
 export const PROTOCOL = env === 'development' ? 'http://' : 'http://'
